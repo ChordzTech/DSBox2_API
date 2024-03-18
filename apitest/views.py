71d8b941ebbddf6d371d0eb04ef8ee5518547895
Prajwal-2151 <prajwalpunekar9565@gmail.com>
2024-03-18 16:58:54 +0530

    delete client method updated

--- apitest/views.py
+++ apitest/views.py
@@ -937,11 +937,11 @@ class ClientDetailsAPI(ModelViewSet):
             if Estimatedetails.objects.filter(clientid=instance.clientid).exists():
                 error_message = "Client has estimates so it cant be deleted"
                 error_response = {
-                    "status": "success",
-                    "code": status.HTTP_400_BAD_REQUEST,
+                    "status": "error",
+                    "code": status.HTTP_200_OK,
                     "message": error_message,
                 }
-                return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+                return Response(error_response, status=status.HTTP_200_OK)
 
             # If no associated records found, proceed with deletion
             instance.delete()
